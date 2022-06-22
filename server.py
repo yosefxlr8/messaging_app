fde4cbf9b22b12daeea7357daf7089cd50e3fc10
yosefxlr8 <108019456+yosefxlr8@users.noreply.github.com>
2022-06-22 19:15:16 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,21 +1,25 @@
 import socket
 
-# Buying a phone and choosing it's type precisely for the need
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Insert the Sim card (bind the phone into a phone number)
 server_socket.bind((socket.gethostname(), 1234))
 
-# listening or waiting for someone to call me
 server_socket.listen(5)
 
-# accepting the call and send some information
-# and then close the phone call and wait for another and repeat
 while True:
-    client_socket, address = server_socket.accept()
-    print(client_socket.recv(10024).decode('utf-8'))
-    msg ='Hello Josh How are you today?'.encode('UTF-8')
-    client_socket.send(msg)
-    client_socket.close()
+    socket1, address1 = server_socket.accept()
+    print(f'Established connection with {address1}')
+    socket1.send('Send a message'.encode('utf-8'))
 
+    socket2, address2 = server_socket.accept()
+    print(f'Established connection with {address1}')
+
+    while True:
+        msg = socket1.recv(1024)
+        socket2.send(msg)
+
+        msg = socket2.recv(1024)
+        socket1.send(msg)
+
+    socket1.close()
+    socket2.close()
 
